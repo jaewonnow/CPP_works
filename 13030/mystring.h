515046365eb5de1446a4a11e7b27511d91075b03
J.G <54757200+jingul414@users.noreply.github.com>
2023-11-03 20:08:39 +0900

    13030

---
+++ 13030/mystring.h
@@ -0,0 +1,17 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+class MyString {
+	// 공개 메소드
+	public:	
+		MyString();  // 기본 생성자.
+		MyString(const char * string);
+		void add(MyString * data);
+		void add(const char* string);
+		void print();
+		int length();
+		~MyString();
+	// 멤버 변수 및 비공개 함수
+	private:
+		char * word_;
+};
+#endif //_MYSTRING_H_
